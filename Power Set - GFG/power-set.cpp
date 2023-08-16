775e882d38a6c9965b55a5aa3588ab6a8d79e9d0
Akshayacse1010 <116668612+Akshayacse1010@users.noreply.github.com>
2023-08-16 13:21:54 +0530

    Added solution - LeetHub

---
+++ Power Set - GFG/power-set.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		vector<string> AllPossibleStrings(string s){
+		    // Code here
+// 		     vector<string>v;
+//         int n = s.size();
+
+//         for(int i=0;i<=(1<<n)-1;i++){
+//             string p="";
+//             for(int j=0;j<n;j++){
+//                 if(i & (1<<j)){
+//                   p+=s[i];
+//                 }
+//             }
+//             v.push_back(p);
+//         }
+//         return v;
+// 		}
+int n = s.length();
+	vector<string>ans;
+	for (int num = 0; num < (1 << n); num++) {
+		string sub = "";
+		for (int i = 0; i < n; i++) {
+			//check if the ith bit is set or not
+			if (num & (1 << i)) {
+				sub += s[i];
+			}
+		}
+		if (sub.length() > 0) {
+			ans.push_back(sub);
+		}
+	}
+	sort(ans.begin(), ans.end());
+	return ans;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string s;
+		cin >> s;
+		Solution ob;
+		vector<string> res = ob.AllPossibleStrings(s);
+		for(auto i : res)
+			cout << i <<" ";
+		cout << "\n";
+
+	}
+	return 0;
+}
+// } Driver Code Ends
