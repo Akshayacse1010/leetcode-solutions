7aa4401944e8a9b1e086f824407d83534409deb4
Akshayacse1010 <116668612+Akshayacse1010@users.noreply.github.com>
2023-09-24 20:21:39 +0530

    Time: 1 ms (97.07%), Space: 41.5 MB (83.86%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -2,11 +2,8 @@ class Solution {
     public int findNumbers(int[] nums) {
       int sum = 0;
       for(int x : nums){
-          int c = 0;
-          while(x > 0){
-              c++;
-              x = x/10;
-          }
+
+   int c = (int)Math.floor(Math.log10(x) + 1);
           if(c %2 == 0){
               sum++;
           }
