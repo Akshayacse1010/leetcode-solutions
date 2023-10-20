23711ec4f888b3992192874b9d4617de1022712e
Akshayacse1010 <116668612+Akshayacse1010@users.noreply.github.com>
2023-10-20 20:19:40 +0530

    Time: 0 ms (100%), Space: 42.8 MB (60.85%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,62 +1,34 @@
-public class Solution {
-    public boolean search(int[] nums, int target) {
-        if (nums[0] == target || nums[nums.length - 1] == target) {
-            return true;
-        }
-
-        int n = nums.length;
-        int l = 0;
-        int h = n - 1;
-        while (l + 1 < n && nums[l] == nums[l + 1]) {
-            l++;
-        }
-
-        if (l == n - 1) {
-            return nums[0] == target;
-        }
-
-        while (h >= 0 && nums[h] == nums[0]) {
-            h--;
-        }
-
-        int start = l;
-        int end = h;
-
-        int pivot = -1;
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            if (nums[mid] >= nums[0]) {
-                l = mid + 1;
-            } else {
-                pivot = mid;
-                h = mid - 1;
-            }
-        }
-
-        if (pivot == -1) {
-            l = start;
-            h = end;
-        } else {
-            if (target > nums[end]) {
-                l = start;
-                h = pivot - 1;
-            } else {
-                l = pivot;
-                h = end;
-            }
-        }
-
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-            if (nums[mid] > target) {
-                h = mid - 1;
-            } else if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0 , high = n-1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(arr[mid]== target){
                 return true;
             }
+            if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+                low++;
+                high--;
+               continue; 
+            }
+            if(arr[low] <= arr[mid]){
+                if(arr[low] <= target && arr[mid] >= target){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else{
+                if(arr[high] >= target && arr[mid] <= target){
+                    low = mid +1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
         }
-
         return false;
     }
 }
